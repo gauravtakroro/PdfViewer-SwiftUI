f25d3ba21ee0b30695a57341808e06dd77989fae
Gaurav Tak <gaurav.tak@roro.io>
2023-12-31 03:26:49 +0530

    Initial Commit

---
+++ PdfViewer-SwiftUI/PdfViewer_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PdfViewer_SwiftUIApp.swift
+//  PdfViewer-SwiftUI
+//
+//  Created by Roro Solutions LLP on 31/12/23.
+//
+
+import SwiftUI
+
+@main
+struct PdfViewer_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
