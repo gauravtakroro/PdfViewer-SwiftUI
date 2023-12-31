58f5b18c4bfd91dcc46cca1338aaa49db3612775
Gaurav Tak <gaurav.tak@roro.io>
2023-12-31 14:35:16 +0530

    folder structure added

--- PdfViewer-SwiftUI/ContentView.swift
+++ PdfViewer-SwiftUI/ContentView.swift
@@ -10,10 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "book")
                 .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+                .foregroundColor(.primary)
+            Text("Pdf Viewer")
         }
         .padding()
     }
